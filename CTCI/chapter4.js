cb1e4b8bc75b388ae459e7baf16dd0b9418095e5
Jeffrey Chan <jefrayc@gmail.com>
2018-10-01 17:13:42 -0700

    Add tree

--- CTCI/chapter4.js
+++ CTCI/chapter4.js
@@ -1,3 +1,17 @@
-function isBalanced(root){
-    
+const BSTNode = require('./bst_node');
+class BinarySearchTree {
+    constructor() {
+        this.root = nil;
+    }
+
+    insert(val) {
+        if(!this.root) {
+            this.root = new BinaryTreeNode(val);
+            return;
+        } 
+
+    }
 }
+
+const search = new BinarySearchTree();
+search.insert(5);
