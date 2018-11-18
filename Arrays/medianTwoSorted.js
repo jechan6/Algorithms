78d5bbab4eaf0a16a2504107b48be2c55349e752
Jeffrey Chan <chanmjeffrey@gmail.com>
2018-11-17 18:36:52 -0800

    Better median of two sorted array

--- Arrays/medianTwoSorted.js
+++ Arrays/medianTwoSorted.js
@@ -30,3 +30,37 @@ var findMedianSortedArrays = function(nums1, nums2) {
         return parseInt(result[mid]);
     }
 };
+//O(log(min(m,n)))
+var findMedianSortedArrays = function(nums1, nums2) {
+    if(nums2.length < nums1.length) {
+        return findMedianSortedArrays(nums2, nums1);
+    }
+    let x = nums1.length;
+    let y = nums2.length;
+    let left = 0;
+    let right = x;
+    while(left <= right) {
+        let partitionX = Math.floor((left + right)/2);
+        let partitionY = Math.floor((x+y+1)/2 - partitionX);
+        
+        let maxLeftX = partitionX === 0 ? Number.MIN_SAFE_INTEGER : nums1[partitionX - 1];
+        let minRightX = partitionX === x ? Number.MAX_SAFE_INTEGER : nums1[partitionX];
+        
+        let maxLeftY = partitionY === 0 ?  Number.MIN_SAFE_INTEGER  : nums2[partitionY - 1];
+        let minRightY = partitionY === y ? Number.MAX_SAFE_INTEGER : nums2[partitionY];
+        
+        if(maxLeftX <= minRightY && maxLeftY <= minRightX) {
+            
+            if((x + y) % 2 === 0) {
+                return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2;
+            } else {
+                
+                return Math.max(maxLeftX, maxLeftY);
+            } 
+        } else if(maxLeftX > minRightY) {
+            right = partitionX - 1;
+        } else {
+            left = partitionX + 1;
+        }
+    }
+};
